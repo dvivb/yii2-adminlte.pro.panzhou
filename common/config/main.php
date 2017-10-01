c1a8754a637b48a9619ca82badba9ef05b088f92
Clown <mn1@live.cn>
2017-10-02 03:11:58 +0800

    create

---
+++ common/config/main.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=yii2-adminlte-master',
+            'username' => 'root',
+            'password' => 'root',
+            'charset' => 'utf8',
+        ],
+    ],
+];
