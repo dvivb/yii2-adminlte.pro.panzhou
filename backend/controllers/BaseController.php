4782828962a91876348ad861983dfc680b2f618b
Clown <mn1@live.cn>
2017-11-11 17:25:03 +0800

    modify upload

--- backend/controllers/BaseController.php
+++ backend/controllers/BaseController.php
@@ -46,4 +46,225 @@ class BaseController extends Controller
         }
          
     }
+
+    public static function uploadImage($uplod_file)
+    {
+        // 文件存放路径
+        $path = yii::$app->basePath . '\web\upload\image\\';
+
+        // 允许上传的图片后缀
+        $allowedExts = array("gif", "jpeg", "jpg", "png");
+        $temp = explode(".", $uplod_file["file"]["name"]);
+
+//        echo $uplod_file["file"]["size"];
+
+        $data = [
+            "code"      => 0,
+            "message"   => ""
+        ];
+
+        $extension = end($temp);     // 获取文件后缀名
+        if ((($uplod_file["file"]["type"] == "image/gif")
+                || ($uplod_file["file"]["type"] == "image/jpeg")
+                || ($uplod_file["file"]["type"] == "image/jpg")
+                || ($uplod_file["file"]["type"] == "image/pjpeg")
+                || ($uplod_file["file"]["type"] == "image/x-png")
+                || ($uplod_file["file"]["type"] == "image/png"))
+            && ($uplod_file["file"]["size"] < 204800)   // 小于 200 kb
+            && in_array($extension, $allowedExts))
+        {
+            if ($uplod_file["file"]["error"] > 0) {
+//                echo "错误：: " . $uplod_file["file"]["error"] . "<br>";
+                $data = [
+                    "code"      => 0,
+                    "message"   => "错误：" . $uplod_file["file"]["error"]
+                ];
+            } else {
+//                echo "上传文件名: " . $uplod_file["file"]["name"] . "<br>";
+//                echo "文件类型: " . $uplod_file["file"]["type"] . "<br>";
+//                echo "文件大小: " . ($uplod_file["file"]["size"] / 1024) . " kB<br>";
+//                echo "文件临时存储的位置: " . $uplod_file["file"]["tmp_name"] . "<br>";
+
+                // 判断当期目录下的 upload 目录是否存在该文件
+                // 如果没有 upload 目录，你需要创建它，upload 目录权限为 777
+                if (file_exists($path . $uplod_file["file"]["name"]))
+                {
+                    $data = [
+                        "code"      => 0,
+                        "message"   => "错误：" . $uplod_file["file"]["name"] . " 文件已经存在！"
+                    ];
+                } else {
+                    // 如果 upload 目录不存在该文件则将文件上传到 upload 目录下
+                    // move_uploaded_file($uplod_file["file"]["tmp_name"], "upload/" . $uplod_file["file"]["name"]);
+                    if (move_uploaded_file($uplod_file["file"]["tmp_name"], $path . $uplod_file["file"]["name"])){
+                        $data = [
+                            "code"      => 1,
+                            "message"   => "成功：" . $uplod_file["file"]["name"] . " 文件上传成功！",
+                            "path"      => $path,
+                            "name"      => $uplod_file["file"]["name"],
+                        ];
+                    }else{
+                        $data = [
+                            "code"      => 0,
+                            "message"   => "错误：" . $uplod_file["file"]["name"] . " 文件上传失败！"
+                        ];
+                    }
+                }
+            }
+        } else {
+            $data = [
+                "code"      => 0,
+                "message"   => "错误：文件格式不支持！"
+            ];
+        }
+
+        return $data;
+    }
+
+    public static function uploadExcel($uplod_file)
+    {
+        // 文件存放路径
+        $path = yii::$app->basePath . '\web\upload\excel\\';
+
+        // 允许上传的图片后缀
+        $allowedExts = array("csv", "xls", "xlsx");
+        $temp = explode(".", $uplod_file["file"]["name"]);
+
+//        echo $uplod_file["file"]["size"];
+
+        $data = [
+            "code"      => 0,
+            "message"   => ""
+        ];
+
+        $extension = end($temp);     // 获取文件后缀名
+        if ((($uplod_file["file"]["type"] == "application/vnd.ms-excel")
+                || ($uplod_file["file"]["type"] == "application/vnd.ms-excel")
+                || ($uplod_file["file"]["type"] == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"))
+            && ($uplod_file["file"]["size"] < 2048000)   // 小于 200 M
+            && in_array($extension, $allowedExts))
+        {
+            if ($uplod_file["file"]["error"] > 0) {
+//                echo "错误：: " . $uplod_file["file"]["error"] . "<br>";
+                $data = [
+                    "code"      => 0,
+                    "message"   => "错误：" . $uplod_file["file"]["error"]
+                ];
+            } else {
+//                echo "上传文件名: " . $uplod_file["file"]["name"] . "<br>";
+//                echo "文件类型: " . $uplod_file["file"]["type"] . "<br>";
+//                echo "文件大小: " . ($uplod_file["file"]["size"] / 1024) . " kB<br>";
+//                echo "文件临时存储的位置: " . $uplod_file["file"]["tmp_name"] . "<br>";
+
+                // 判断当期目录下的 upload 目录是否存在该文件
+                // 如果没有 upload 目录，你需要创建它，upload 目录权限为 777
+                if (file_exists($path . $uplod_file["file"]["name"]))
+                {
+                    $data = [
+                        "code"      => 0,
+                        "message"   => "错误：" . $uplod_file["file"]["name"] . " 文件已经存在！"
+                    ];
+                } else {
+                    // 如果 upload 目录不存在该文件则将文件上传到 upload 目录下
+                    // move_uploaded_file($uplod_file["file"]["tmp_name"], "upload/" . $uplod_file["file"]["name"]);
+                    if (move_uploaded_file($uplod_file["file"]["tmp_name"], $path . $uplod_file["file"]["name"])){
+                        $data = [
+                            "code"      => 1,
+                            "message"   => "成功：" . $uplod_file["file"]["name"] . " 文件上传成功！",
+                            "path"      => $path,
+                            "name"      => $uplod_file["file"]["name"],
+                        ];
+                    }else{
+                        $data = [
+                            "code"      => 0,
+                            "message"   => "错误：" . $uplod_file["file"]["name"] . " 文件上传失败！"
+                        ];
+                    }
+                }
+            }
+        } else {
+            $data = [
+                "code"      => 0,
+                "message"   => "错误：文件格式不支持！"
+            ];
+        }
+
+        return $data;
+    }
+
+    public static function uploadFile($uplod_file)
+    {
+        // 文件存放路径
+        $path = yii::$app->basePath . '\web\upload\file\\';
+
+        // 允许上传的图片后缀
+        $allowedExts = array("gif", "jpeg", "jpg", "png", "csv", "xls", "xlsx");
+        $temp = explode(".", $uplod_file["file"]["name"]);
+
+//        echo $uplod_file["file"]["size"];
+
+        $data = [
+            "code"      => 0,
+            "message"   => ""
+        ];
+
+        $extension = end($temp);     // 获取文件后缀名
+        if ((($uplod_file["file"]["type"] == "image/gif")
+                || ($uplod_file["file"]["type"] == "image/jpeg")
+                || ($uplod_file["file"]["type"] == "image/jpg")
+                || ($uplod_file["file"]["type"] == "image/pjpeg")
+                || ($uplod_file["file"]["type"] == "image/x-png")
+                || ($uplod_file["file"]["type"] == "image/png")
+                || ($uplod_file["file"]["type"] == "application/vnd.ms-excel")
+                || ($uplod_file["file"]["type"] == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"))
+            && ($uplod_file["file"]["size"] < 222204800)   // 小于 200 kb
+            && in_array($extension, $allowedExts))
+        {
+            if ($uplod_file["file"]["error"] > 0) {
+//                echo "错误：: " . $uplod_file["file"]["error"] . "<br>";
+                $data = [
+                    "code"      => 0,
+                    "message"   => "错误：" . $uplod_file["file"]["error"]
+                ];
+            } else {
+//                echo "上传文件名: " . $uplod_file["file"]["name"] . "<br>";
+//                echo "文件类型: " . $uplod_file["file"]["type"] . "<br>";
+//                echo "文件大小: " . ($uplod_file["file"]["size"] / 1024) . " kB<br>";
+//                echo "文件临时存储的位置: " . $uplod_file["file"]["tmp_name"] . "<br>";
+
+                // 判断当期目录下的 upload 目录是否存在该文件
+                // 如果没有 upload 目录，你需要创建它，upload 目录权限为 777
+                if (file_exists($path . $uplod_file["file"]["name"]))
+                {
+                    $data = [
+                        "code"      => 0,
+                        "message"   => "错误：" . $uplod_file["file"]["name"] . " 文件已经存在！"
+                    ];
+                } else {
+                    // 如果 upload 目录不存在该文件则将文件上传到 upload 目录下
+                    // move_uploaded_file($uplod_file["file"]["tmp_name"], "upload/" . $uplod_file["file"]["name"]);
+                    if (move_uploaded_file($uplod_file["file"]["tmp_name"], $path . $uplod_file["file"]["name"])){
+                        $data = [
+                            "code"      => 1,
+                            "message"   => "成功：" . $uplod_file["file"]["name"] . " 文件上传成功！",
+                            "path"      => $path,
+                            "name"      => $uplod_file["file"]["name"],
+                        ];
+                    }else{
+                        $data = [
+                            "code"      => 0,
+                            "message"   => "错误：" . $uplod_file["file"]["name"] . " 文件上传失败！"
+                        ];
+                    }
+                }
+            }
+        } else {
+            $data = [
+                "code"      => 0,
+                "message"   => "错误：文件格式不支持！"
+            ];
+        }
+
+        return $data;
+    }
 }
--- backend/modules/project/controllers/ProjectController.php
+++ backend/modules/project/controllers/ProjectController.php
@@ -130,116 +130,30 @@ class ProjectController extends BaseController
         var_dump($_GET);
     }
 
-
-//    public function actionUpload()
-//    {
-//        $model = new UploadForm();
-//        echo "文件临时存储的位置: " . $_FILES["imageFile"]["tmp_name"] . "<br>";
-//var_dump($_POST);die;
-//        if (Yii::$app->request->isPost) {
-//            $model->imageFile = UploadedFile::getInstance($model, 'imageFile');
-//            if ($model->upload()) {
-//                // 文件上传成功
-//                return;
-//            }
-//            return $model->upload();
-//        }
-//
-////        $data = new ProjectsSearch();
-////        $projectSearch = $data->search(Yii::$app->request->queryParams);
-////        return $this->render('index',['data'=>$projectSearch,'searchModel'=> $data]);
-////        return $this->render('index', ['model' => $model]);
-//    }
-
-
     public function actionUpload()
     {
-        $data = [
-            "code"      => -1,
-            "message"   => ""
-        ];
-
-        // 允许上传的图片后缀
-        $allowedExts = array("gif", "jpeg", "jpg", "png", "csv", "xls", "xlsx");
-        $temp = explode(".", $_FILES["file"]["name"]);
-//        echo $_FILES["file"]["size"];
-
-        $extension = end($temp);     // 获取文件后缀名
-        if ((($_FILES["file"]["type"] == "image/gif")
-                || ($_FILES["file"]["type"] == "image/jpeg")
-                || ($_FILES["file"]["type"] == "image/jpg")
-                || ($_FILES["file"]["type"] == "image/pjpeg")
-                || ($_FILES["file"]["type"] == "image/x-png")
-                || ($_FILES["file"]["type"] == "image/png")
-                || ($_FILES["file"]["type"] == "application/vnd.ms-excel")
-                || ($_FILES["file"]["type"] == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"))
-            && ($_FILES["file"]["size"] < 222204800)   // 小于 200 kb
-            && in_array($extension, $allowedExts))
-        {
-            if ($_FILES["file"]["error"] > 0)
-            {
-//                echo "错误：: " . $_FILES["file"]["error"] . "<br>";
+        $data = self::uploadExcel($_FILES);
+        if($data["code"]){
+            $path = $data["path"];
+            $name = $data["name"];
+            $import = ProjectService::importProject($path, $name);
+            if ($import){
                 $data = [
-                    "code"      => -1,
-                    "message"   => "错误：" . $_FILES["file"]["error"]
+                    "code"      => 1,
+                    "message"   => "成功：" . $_FILES["file"]["name"] . " 文件导入成功！"
+                ];
+            }else{
+                $data = [
+                    "code"      => 0,
+                    "message"   => "错误：" . $_FILES["file"]["name"] . " 文件数据错误！"
                 ];
             }
-            else
-            {
-//                echo "上传文件名: " . $_FILES["file"]["name"] . "<br>";
-//                echo "文件类型: " . $_FILES["file"]["type"] . "<br>";
-//                echo "文件大小: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-//                echo "文件临时存储的位置: " . $_FILES["file"]["tmp_name"] . "<br>";
-
-                // 判断当期目录下的 upload 目录是否存在该文件
-                // 如果没有 upload 目录，你需要创建它，upload 目录权限为 777
-                if (file_exists("upload/" . $_FILES["file"]["name"]))
-                {
-//                    echo $_FILES["file"]["name"] . " 文件已经存在!";
-                    $data = [
-                        "code"      => -1,
-                        "message"   => "错误：" . $_FILES["file"]["name"] . " 文件已经存在！"
-                    ];
-                }
-                else
-                {
-                    // 如果 upload 目录不存在该文件则将文件上传到 upload 目录下
-                    move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $_FILES["file"]["name"]);
-//                    echo "文件存储在: " . "upload/" . $_FILES["file"]["name"];
-
-                    $path = yii::$app->basePath . '\web\upload\\';
-                    $name = $_FILES["file"]["name"];
-
-                    $import = ProjectService::importProject($path, $name);
-                    if ($import){
-                        $data = [
-                            "code"      => -1,
-                            "message"   => "成功：" . $_FILES["file"]["name"] . " 文件导入成功！"
-                        ];
-                    }else{
-                        $data = [
-                            "code"      => -1,
-                            "message"   => "错误：" . $_FILES["file"]["name"] . " 文件数据错误！"
-                        ];
-                    }
-                }
-            }
         }
-        else
-        {
-//            echo "非法的文件格式";
-            $data = [
-                "code"      => -1,
-                "message"   => "错误：文件格式不支持！"
-            ];
-        }
-
         if($data["code"]){
             Yii::$app->getSession()->setFlash('success', $data["message"]);
         }else{
-            Yii::$app->getSession()->setFlash('success', $data["message"]);
+            Yii::$app->getSession()->setFlash('error', $data["message"]);
         }
-        return $this->redirect(['/project/project']);
+        return $this->redirect(['/project/project/index']);
     }
-
 }
